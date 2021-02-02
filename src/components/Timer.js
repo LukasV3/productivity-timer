dee5a1e35a4488934edef43b8e4f17f16393de48
LukasV3 <volk.lukas237@yahoo.co.uk>
2021-02-02 19:10:10 +0000

    began implementing countdown functionality

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import _ from "lodash";
 
 import { updateActiveMenu } from "../actions";
 
 const Timer = (props) => {
+  const [time, setTime] = useState("");
+  const [timerActive, setTimerActive] = useState(false);
+
   const onMenuClick = (menu) => {
     if (props.activeMenu === menu) return;
     props.updateActiveMenu(menu);
@@ -21,13 +24,41 @@ const Timer = (props) => {
     });
   };
 
+  const startCountdown = (duration) => {
+    setTimerActive(true);
+
+    const start = Date.now();
+
+    function timer() {
+      let diff = duration * 60 - (((Date.now() - start) / 1000) | 0);
+
+      let minutes = (diff / 60) | 0;
+      let seconds = diff % 60 | 0;
+
+      // minutes = minutes < 10 ? `0${minutes}` : minutes;
+      seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+      setTime(`${minutes}:${seconds}`);
+      if (minutes === "00" && seconds === "00") {
+        clearInterval(interval);
+      }
+    }
+    timer();
+    const interval = setInterval(timer, 1000);
+  };
+
   return (
     <div className="ui center aligned container" style={{ marginTop: "20px" }}>
       <div className="ui basic buttons">{renderMenuButtons()}</div>
       <div className="ui header" style={{ fontSize: "6rem", margin: "30px" }}>
-        {props.settings[props.activeMenu]}:00
+        {timerActive ? time : `${props.settings[props.activeMenu]}:00`}
       </div>
-      <button className="ui huge basic black button">START</button>
+      <button
+        onClick={() => startCountdown(props.settings[props.activeMenu])}
+        className="ui huge basic black button"
+      >
+        {timerActive ? "STOP" : "START"}
+      </button>
     </div>
   );
 };
